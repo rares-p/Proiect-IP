d73fe84e3449763ac714ab3e61c66ba47e802a0d
cristian-roman <cristian.roman@feel-it-services.com>
2023-05-01 10:40:44 +0300

    get all, create and delete lobby tested

--- Town-Of-Salem/src/test/java/com/github/raresp/proiectip/TownOfSalem/API/LobbyAPISteps.java
+++ Town-Of-Salem/src/test/java/com/github/raresp/proiectip/TownOfSalem/API/LobbyAPISteps.java
@@ -1,9 +1,99 @@
 package com.github.raresp.proiectip.TownOfSalem.API;
 
-import io.cucumber.java.en.Given;
+import HttpRequestTestClient.Connection;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
+import java.net.http.HttpResponse;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class LobbyAPISteps {
+
+    private final Connection connection = new Connection();
+    HttpResponse<String> response;
+
     public LobbyAPISteps() {
+    }
+
+    @When("I send a GET request to \"lobbies\"")
+    public void iSendAGETRequestToLobbies() {
+        try {
+            response = connection.get("/lobbies");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Then("the response status code should be {int}")
+    public void theResponseStatusCodeShouldBe(int statusCode) {
+        if (response.statusCode() != statusCode) {
+            throw new RuntimeException("Response code is not " + statusCode + " but " + response.statusCode());
+        }
+    }
+
+    @And("the response should be a JSON having the following keys: {string}, {string}, {string};")
+    public void theResponseShouldBeAJSONHavingTheFollowingKeys(String id, String state, String waitingToJoin) {
+        String body = response.body();
+
+        if(!body.contains("[]"))
+        {
+            Pattern pattern = Pattern.compile("\\[(\\s*\\{\\s*\"id\":\\s*\"[0-9a-z]{8}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{12}\",\\s*\"state\":\\s*\"(WAITING_PLAYERS|STARTED|ENDED)\",\\s*\"waitingToJoin\":\\s*\\[((\\s*\".*\",\\n)*\\s*\".*\")?\\s*]\\s*}(,\\s*\\{\\s*\"id\":\\s*\"[0-9a-z]{8}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{12}\",\\s*\"state\":\\s*\"(WAITING_PLAYERS|STARTED|ENDED)\",\\s*\"waitingToJoin\":\\s*\\[((\\s*\".*\",\\n)*\\s*\".*\")?\\s*]\\s*})*)?\\s*]");
+            Matcher matcher = pattern.matcher(body);
+            if(!matcher.matches())
+            {
+                throw new RuntimeException("Response body does not match the expected format");
+            }
+        }
+    }
+
+    @When("I send a POST request to \"lobbies\"")
+    public void iSendAPOSTRequestTo() {
+        try {
+            response = connection.post("/lobbies", null);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @And("the response should be a JSON having the following keys: {string}, {string}, {string}, {string}, {string};")
+    public void theResponseShouldBeAJSONHavingTheFollowingKeys(String id, String game, String waitingToJoin, String state, String minimumPlayers) {
+
+        var bodyResponse = response.body();
+
+        Pattern p = Pattern.compile("\\{\\s*\"id\":\\s*\"[0-9a-z]{8}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{12}\",\\s*\"game\":\\s*null,\\s*\"waitingToJoin\":\\s*\\[],\\s*\"state\":\\s*\"WAITING_PLAYERS\",\\s*\"minimumPlayers\":\\s*[0-9]}");
+
+        Matcher m = p.matcher(response.body());
+        if(!m.matches())
+        {
+            throw new RuntimeException("Response body does not match the expected format");
+        }
+
+    }
+
+    @Then("I delete the lobby")
+    public void iDeleteTheLobby() {
+        try {
+            var lastCreatedLobbyId = getLastResponseId();
+            response = connection.delete("/lobbies", lastCreatedLobbyId);
+        } catch (Exception e) {
+            throw new RuntimeException("Could not delete lobby with id " + getLastResponseId() + " because of " + e.getMessage());
+        }
+    }
+
+    public String getLastResponseId()
+    {
+        Pattern p = Pattern.compile("\\s*\"id\":\\s*\"([0-9a-z]{8}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{12})\"");
+        Matcher m = p.matcher(response.body());
+
+        if(m.find())
+        {
+            return m.group(1);
+        }
+        else
+        {
+            throw new RuntimeException("Last response does not contain a valid id");
+        }
     }
 }
--- Town-Of-Salem/src/test/resources/LobbyAPI.feature
+++ Town-Of-Salem/src/test/resources/LobbyAPI.feature
@@ -1,15 +1,21 @@
 Feature: Lobby endpoints
   Test the endpoints in the lobby service:
-    POST: /lobbies --- creates a new lobby
-    DELETE: /lobbies/{id} --- deletes a specific lobby
+  POST: /lobbies --- creates a new lobby
+  DELETE: /lobbies/{id} --- deletes a specific lobby
 
-    POST: /lobbies/{id}/add_user --- adds a user to a specific lobby
+  POST: /lobbies/{id}/add_user --- adds a user to a specific lobby
 
-    POST: /lobbies/{id}/start_game --- starts a game in a specific lobby
-
+  POST: /lobbies/{id}/start_game --- starts a game in a specific lobby
 
 
   Scenario: Get all lobbies
-    Given I have a list of lobbies
-    When I send a GET request to "/lobbies"
+    When I send a GET request to "lobbies"
     Then the response status code should be 200
+    And the response should be a JSON having the following keys: "id", "state", "waitingToJoin";
+
+    Scenario: Create a lobby and delete lobby
+      When I send a POST request to "lobbies"
+      Then the response status code should be 201
+      And the response should be a JSON having the following keys: "id", "game", "waitingToJoin", "state", "minimumPlayers";
+      Then I delete the lobby
+      And the response status code should be 200

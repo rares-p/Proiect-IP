7553eaf34857af417f7d48c7ccf3d15a06b3895a
rares-p <panainte.rares02@gmail.com>
2023-05-28 14:48:18 +0300

    minor role changes

--- Town-Of-Salem/src/main/java/com/github/raresp/proiectip/TownOfSalem/utils/GameUtils.java
+++ Town-Of-Salem/src/main/java/com/github/raresp/proiectip/TownOfSalem/utils/GameUtils.java
@@ -4,9 +4,7 @@ import com.github.raresp.proiectip.TownOfSalem.exceptions.InvalidLobbyException;
 import com.github.raresp.proiectip.TownOfSalem.models.Lobby;
 import com.github.raresp.proiectip.TownOfSalem.models.characters.Character;
 import com.github.raresp.proiectip.TownOfSalem.models.characters.MafiaCharacters.*;
-import com.github.raresp.proiectip.TownOfSalem.models.characters.NeutralCharacters.Arsonist;
-import com.github.raresp.proiectip.TownOfSalem.models.characters.NeutralCharacters.SerialKiller;
-import com.github.raresp.proiectip.TownOfSalem.models.characters.NeutralCharacters.Survivor;
+import com.github.raresp.proiectip.TownOfSalem.models.characters.NeutralCharacters.*;
 import com.github.raresp.proiectip.TownOfSalem.models.characters.TownCharacters.*;
 
 import java.util.ArrayList;
@@ -32,97 +30,66 @@ public class GameUtils {
         boolean randomBool1 = random.nextBoolean();
         int randomInt1 = random.nextInt(3);
 
+
+        for (int i = 0; i < numberOfTownPlayers; i++) {
+            switch (i) {
+                case 0 -> characters.add(new Bodyguard(playerNames.get(0)));
+                case 1 -> characters.add(new Veteran(playerNames.get(0)));
+                case 2 -> characters.add(new Doctor(playerNames.get(0)));
+                case 3 -> characters.add(new Sheriff(playerNames.get(0)));
+                case 4 -> characters.add(new Escort(playerNames.get(0)));
+                default -> {
+                    int randomRole = random.nextInt(6);
+                    switch (randomRole) {
+                        case 0 -> characters.add(new Vigilante(playerNames.get(0)));
+                        case 1 -> characters.add(new Spy(playerNames.get(0)));
+                        case 2 -> characters.add(new Lookout(playerNames.get(0)));
+                        case 3 -> characters.add(new Doctor(playerNames.get(0)));
+                        case 4 -> characters.add(new Escort(playerNames.get(0)));
+                        case 5 -> characters.add(new Sheriff(playerNames.get(0)));
+                        default -> characters.add(new Sheriff(playerNames.get(0)));
+                    }
+                }
+            }
+            playerNames.remove(playerNames.get(0));
+        }
+
         for (int i = 0; i < numberOfNeutralPlayers; i++) {
             switch (i) {
                 case 0 -> {
-                    characters.add(new Arsonist(playerNames.get(0)));
-                    playerNames.remove(playerNames.get(0));
+                    Executioner executioner = new Executioner(playerNames.get(0));
+                    int randomTarget = random.nextInt(numberOfNeutralPlayers);
+                    executioner.target = characters.get(randomTarget);
+                    characters.add(executioner);
                 }
                 case 1 -> {
-                    if (randomBool1)
-                        characters.add(new SerialKiller(playerNames.get(0)));
-                    else
-                        characters.add(new Survivor(playerNames.get(0)));
-                    playerNames.remove(playerNames.get(0));
+                    int randomNeutral = random.nextInt(5);
+                    switch (randomNeutral) {
+                        case 0 -> characters.add(new Arsonist(playerNames.get(0)));
+                        case 1 -> characters.add(new Jester(playerNames.get(0)));
+                        case 2 -> characters.add(new SerialKiller(playerNames.get(0)));
+                        case 3 -> characters.add(new Werewolf(playerNames.get(0)));
+                        case 4 -> characters.add(new Survivor(playerNames.get(0)));
+                    }
                 }
             }
+            playerNames.remove(playerNames.get(0));
         }
 
         for (int i = 0; i < numberOfMafiaPlayers; i++) {
             switch (i) {
-                case 0 -> {
-                    characters.add(new GodFather(playerNames.get(0)));
-                    playerNames.remove(playerNames.get(0));
-                }
-                case 1 -> {
-                    characters.add(new Mafioso(playerNames.get(0)));
-                    playerNames.remove(playerNames.get(0));
-                }
-                case 2 -> {
-                    characters.add(new Framer(playerNames.get(0)));
-                    playerNames.remove(playerNames.get(0));
-                }
-                case 3 -> {
-                    characters.add(new Consigliere(playerNames.get(0)));
-                    playerNames.remove(playerNames.get(0));
-                }
-                case 4 -> {
-                    characters.add(new Consort(playerNames.get(0)));
-                    playerNames.remove(playerNames.get(0));
+                case 0 -> characters.add(new GodFather(playerNames.get(0)));
+                case 1 -> characters.add(new Mafioso(playerNames.get(0)));
+                default -> {
+                    int randomMafia = random.nextInt(numberOfMafiaPlayers);
+                    switch (randomMafia) {
+                        case 0 -> characters.add(new Framer(playerNames.get(0)));
+                        case 1 -> characters.add(new Consigliere(playerNames.get(0)));
+                        case 2 -> characters.add(new Consort(playerNames.get(0)));
+                    }
                 }
             }
-        }
-
-        for (int i = 0; i < numberOfTownPlayers; i++) {
-            switch (i) {
-                case 0 -> {
-                    characters.add(new Jailor(playerNames.get(0)));
-                    playerNames.remove(playerNames.get(0));
-                }
-                case 1 -> {
-                    characters.add(new Veteran(playerNames.get(0)));
-                    playerNames.remove(playerNames.get(0));
-                }
-                case 2 -> {
-                    characters.add(new Doctor(playerNames.get(0)));
-                    playerNames.remove(playerNames.get(0));
-                }
-                case 3 -> {
-                    characters.add(new Sheriff(playerNames.get(0)));
-                    playerNames.remove(playerNames.get(0));
-                }
-                case 4 -> {
-                    characters.add(new Escort(playerNames.get(0)));
-                    playerNames.remove(playerNames.get(0));
-                }
-                case 5 -> {
-                    if (randomInt1 == 0)
-                        characters.add(new Escort(playerNames.get(0)));
-                    if (randomInt1 == 1)
-                        characters.add(new Sheriff(playerNames.get(0)));
-                    if (randomInt1 == 2)
-                        characters.add(new Vigilante(playerNames.get(0)));
-                    playerNames.remove(playerNames.get(0));
-                }
-                case 6 -> {
-                    if (randomInt1 == 0)
-                        characters.add(new Sheriff(playerNames.get(0)));
-                    if (randomInt1 == 1)
-                        characters.add(new Vigilante(playerNames.get(0)));
-                    if (randomInt1 == 2)
-                        characters.add(new Escort(playerNames.get(0)));
-                    playerNames.remove(playerNames.get(0));
-                }
-                case 7 -> {
-                    if (randomInt1 == 0)
-                        characters.add(new Vigilante(playerNames.get(0)));
-                    if (randomInt1 == 1)
-                        characters.add(new Escort(playerNames.get(0)));
-                    if (randomInt1 == 2)
-                        characters.add(new Sheriff(playerNames.get(0)));
-                    playerNames.remove(playerNames.get(0));
-                }
-            }
+            playerNames.remove(playerNames.get(0));
         }
         return characters;
 

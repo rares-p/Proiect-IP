71a77262a03dd5240458a7973c3df097a6bd53ef
StefanPaulet <tudor.stefan.paulet@gmail.com>
2023-05-28 09:30:31 +0300

    bugfix:arsonist interaction + feature:reset all lobbies endpoint

--- Town-Of-Salem/src/test/java/com/github/raresp/proiectip/TownOfSalem/TownOfSalemApplicationTests.java
+++ Town-Of-Salem/src/test/java/com/github/raresp/proiectip/TownOfSalem/TownOfSalemApplicationTests.java
@@ -4,6 +4,7 @@ import com.github.raresp.proiectip.TownOfSalem.models.TurnInteractions;
 import com.github.raresp.proiectip.TownOfSalem.models.characters.Character;
 import com.github.raresp.proiectip.TownOfSalem.models.characters.MafiaCharacters.GodFather;
 import com.github.raresp.proiectip.TownOfSalem.models.characters.MafiaCharacters.Mafioso;
+import com.github.raresp.proiectip.TownOfSalem.models.characters.NeutralCharacters.Arsonist;
 import com.github.raresp.proiectip.TownOfSalem.models.characters.NeutralCharacters.SerialKiller;
 import com.github.raresp.proiectip.TownOfSalem.models.characters.NeutralCharacters.Werewolf;
 import com.github.raresp.proiectip.TownOfSalem.models.characters.TownCharacters.*;
@@ -18,19 +19,20 @@ import java.util.List;
 class TownOfSalemApplicationTests {
 	@Test
 	void contextLoads() {
-		Bodyguard bodyguard = new Bodyguard("bodyguard");
+		//Bodyguard bodyguard = new Bodyguard("bodyguard");
 		Doctor doctor = new Doctor("doctor");
 		Escort escort = new Escort("escort");
-		GodFather godFather = new GodFather("godfather");
+		/*GodFather godFather = new GodFather("godfather");
 		Lookout lookout = new Lookout("lookout");
 		Mafioso mafioso = new Mafioso("mafioso");
 		SerialKiller serialKiller = new SerialKiller("serial killer");
 		Veteran veteran = new Veteran("veteran");
 		Vigilante vigilante = new Vigilante("vigilante");
-		Werewolf werewolf = new Werewolf("werewolf");
+		Werewolf werewolf = new Werewolf("werewolf");*/
+		Arsonist arsonist = new Arsonist("arsonist");
 
 
-		bodyguard.targets.add(doctor);
+		/*bodyguard.targets.add(doctor);
 		doctor.targets.add(bodyguard);
 		//escort.targets.add(werewolf);
 		//godFather.targets.add(veteran);
@@ -39,18 +41,23 @@ class TownOfSalemApplicationTests {
 		veteran.targets.add(vigilante);
 		//serialKiller.targets.add(doctor);
 		//vigilante.targets.add(doctor);
-		//werewolf.targets.add(doctor);
+		//werewolf.targets.add(doctor);*/
 
-
-		List<Character> characters = List.of(bodyguard, doctor, escort, godFather, lookout, mafioso, serialKiller, veteran, vigilante, werewolf);
-		TurnInteractions turnInteractions = new TurnInteractions(characters, true);
+		arsonist.targets.add(doctor);
+		//List<Character> characters = List.of(bodyguard, doctor, escort, godFather, lookout, mafioso, serialKiller, veteran, vigilante, werewolf);
+		TurnInteractions turnInteractions = new TurnInteractions(List.of(doctor, escort, arsonist), true);
 		turnInteractions.computeInteractionsOutcome();
 
-		for(Character character : characters)
-			System.out.println(character.getPlayerUsername() + ": " + character.nightResults);
+		//for(Character character : characters)
+		//	System.out.println(character.getPlayerUsername() + ": " + character.nightResults);
 
+		arsonist.targets.clear();
+		arsonist.targets.add(arsonist);
 
+		turnInteractions = new TurnInteractions(List.of(doctor, escort, arsonist), true);
+		turnInteractions.computeInteractionsOutcome();
 
+		System.out.println(doctor.isAlive() + doctor.nightResults.toString());
 
 //		vigilante.resetStats();
 //		doctor.resetStats();
